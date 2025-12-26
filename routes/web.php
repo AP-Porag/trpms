f301cb19e1f54ced4d8ad1fe9efa5437ff36f614
AP-Porag <smauhp@gmail.com>
2025-12-27 01:11:49 +0600

    deploy changed

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,19 @@
 <?php
 
 use App\Http\Controllers\Admin\User\UserController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
+Route::get('/execute-command', function () {
+//    return redirect()->route('login');
+//    Artisan::call('storage:link');
+    Artisan::call('migrate:fresh --seed');
+    Artisan::call('cache:clear');
+    Artisan::call('view:clear');
+    Artisan::call('route:clear');
+    Artisan::call('optimize');
+    dd('All commands executed successfully');
+});
 Route::get('/', function () {
     return redirect()->route('login');
 //    return Inertia::render('welcome');
