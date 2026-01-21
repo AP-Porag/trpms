0d12ef995948265adbeb98772b9878e555b12477
AP-Porag <smauhp@gmail.com>
2026-01-21 15:17:27 +0600

    changed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,26 +12,26 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'first_name' => 'Admin',
-            'last_name'  => 'Last',
-            'email'      => 'admin@app.com',
-            'password'   => Hash::make('12345678'),
-            'user_type'  => 'admin',
-            'status'     => true,
-        ]);
-
-        User::create([
-            'first_name' => 'User',
-            'last_name'  => 'Last',
-            'email'      => 'user@app.com',
-            'password'   => Hash::make('12345678'),
-            'user_type'  => 'user',
-            'status'     => true,
-        ]);
-
-        User::factory(5)->create();
-        Client::factory(3)->create();
-        Candidate::factory(10)->create();
+//        User::create([
+//            'first_name' => 'Admin',
+//            'last_name'  => 'Last',
+//            'email'      => 'admin@app.com',
+//            'password'   => Hash::make('12345678'),
+//            'user_type'  => 'admin',
+//            'status'     => true,
+//        ]);
+//
+//        User::create([
+//            'first_name' => 'User',
+//            'last_name'  => 'Last',
+//            'email'      => 'user@app.com',
+//            'password'   => Hash::make('12345678'),
+//            'user_type'  => 'user',
+//            'status'     => true,
+//        ]);
+//
+//        User::factory(5)->create();
+//        Client::factory(3)->create();
+//        Candidate::factory(10)->create();
     }
 }
